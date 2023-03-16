8a0b4215d56ed9a2e844bcef628a2393e3d9de5f
Niklas Nielsen <nik@qni.dk>
2023-03-15 22:40:19 -0700

    Log request and response to API server

--- log10/load.py
+++ log10/load.py
@@ -1,25 +1,52 @@
 from pprint import pprint
-
+import requests
 import openai
+import os
+import json
+
+url = os.environ.get("LOG10_URL")
+token = os.environ.get("LOG10_TOKEN")
+
 orig_completion = openai.Completion.create
+
+
 def intercepted_completion(**params):
-    print("request")
-    pprint(params)
-    output = orig_completion(**params)
-    print("response")
-    pprint(output)
+    session_url = url + "/api/sessions"
+    output = None
+
+    try:
+        res = requests.request("PUT",
+                               session_url, headers={"x-log10-token": token, "Content-Type": "application/json"})
+
+        sessionID = res.json()['sessionID']
+
+        res = requests.request("POST",
+                               session_url + "/" + sessionID, headers={"x-log10-token": token, "Content-Type": "application/json"}, json={
+                                   "request": json.dumps(params)
+                               })
+
+        output = orig_completion(**params)
+
+        res = requests.request("POST",
+                               session_url + "/" + sessionID, headers={"x-log10-token": token, "Content-Type": "application/json"}, json={
+                                   "response": json.dumps(output)
+                               })
+
+    except Exception as e:
+        print(e)
+
     return output
+
 
 openai.Completion.create = intercepted_completion
 
 
 orig_embedding = openai.Embedding.create
+
+
 def intercepted_embedding(**params):
-    print("request")
-    pprint(params)
     output = orig_embedding(**params)
-    print("response")
-    # print(output)
     return output
+
 
 openai.Embedding.create = intercepted_embedding
