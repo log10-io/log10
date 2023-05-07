62c3dde0257d4fe3d9b81bbf85738f395997ea80
Arjun Bansal <mindscience@gmail.com>
2023-05-06 17:10:51 -0700

    add langchain qa example & python-dotenv dependency for builds

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "openai",
+        "python-dotenv",
     ],
     extras_require={
         "bigquery": ["google-cloud-bigquery"],
