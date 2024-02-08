b412b753f9e4ec1dce578cb93a59dc25d07abfe2
Niklas Nielsen <nik@qni.dk>
2024-02-08 07:12:00 -0800

    Added example

---
+++ examples/logging/steaming.py
@@ -0,0 +1,17 @@
+# from openai import OpenAI
+from log10.load import OpenAI
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Count to 200"}],
+    temperature=0,
+    stream=True,  # this time, we set stream=True
+)
+
+for chunk in response:
+    content = chunk.choices[0].delta.content
+    if content:
+        print(content, end="", flush=True)
+print("")
