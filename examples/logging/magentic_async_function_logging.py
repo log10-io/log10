5678269e3b3b452e2e856839284abe5f38fb5bef
Wenzhe Xue <wenzhe@log10.io>
2024-02-22 11:35:15 -0800

    fix response with message

---
+++ examples/logging/magentic_async_function_logging.py
@@ -0,0 +1,34 @@
+import openai
+from magentic import AsyncStreamedStr, FunctionCall, prompt
+
+from log10.load import log10
+
+
+log10(openai)
+
+
+def add(x: int, y: int) -> int:
+    """Add together two numbers."""
+    return x + y
+
+
+@prompt("What is 1+1? Use tools", functions=[add])
+async def agent() -> AsyncStreamedStr:
+    ...
+
+
+# Define an async main function
+async def main():
+    response = await agent()
+    if isinstance(response, FunctionCall):
+        print(response)
+    else:
+        async for chunk in response:
+            print(chunk, end="", flush=True)
+
+
+# Running the main function using asyncio
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
