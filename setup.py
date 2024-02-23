f9299f94912ebbbdba743e18d139078b904669d7
Wenzhe Xue <wenzhe@log10.io>
2024-02-22 16:56:12 -0800

    bump version 0.6.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Log10",
-    version="0.6.4",
+    version="0.6.5",
     description="Log10 LLM data management",
     author="Log10 team",
     author_email="team@log10.io",
