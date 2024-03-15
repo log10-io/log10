327a5e72a587125059b954ec6fcf092e86283509
Wenzhe Xue <wenzhe@log10.io>
2024-03-15 12:06:31 -0700

    use claude-instant-1.2 in anthropic completions example

--- examples/logging/anthropic_completion.py
+++ examples/logging/anthropic_completion.py
@@ -9,7 +9,7 @@ log10(anthropic, DEBUG_=False)
 client = anthropic.Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
 
 response = client.completions.create(
-    model="claude-1",
+    model="claude-instant-1.2",
     prompt=f"\n\nHuman:Write the names of all Star Wars movies and spinoffs along with the time periods in which they were set?{anthropic.AI_PROMPT}",
     temperature=0,
     max_tokens_to_sample=1024,
