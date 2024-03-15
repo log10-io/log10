b71a5de9f18bb3e2a96118f0d2bd9c1d7aaa965c
Wenzhe Xue <wenzhe@log10.io>
2024-03-15 14:29:56 -0700

    release 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Log10",
-    version="0.6.7",
+    version="0.7.0",
     description="Log10 LLM data management",
     author="Log10 team",
     author_email="team@log10.io",
