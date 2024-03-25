a5e3de1197ba5b9da54aa0ef978d06cbd2906701
wenzhe <145375501+wenzhe-log10@users.noreply.github.com>
2024-03-25 16:54:16 -0700

    add vertexai gemini support and refactor the log_row code (#124)
    * wip: add vertexai gemini support and refactor the log_row code
    
    * add example for gemini and bug fix for anthropic messages
    
    * ignore chat history for now
    
    * update status for openai stream

---
+++ examples/logging/vertextai_gemini_chat.py
@@ -0,0 +1,23 @@
+import vertexai
+from vertexai.preview.generative_models import GenerationConfig, GenerativeModel
+
+from log10.load import log10
+
+
+log10(vertexai)
+
+# change these to your own project and location
+project_id = "YOUR_PROJECT_ID"
+location = "YOUR_LOCATION"
+vertexai.init(project=project_id, location=location)
+
+model = GenerativeModel("gemini-1.0-pro")
+chat = model.start_chat()
+
+prompt = "What's the top 5 largest constellations you can find in North American during March?"
+generation_config = GenerationConfig(
+    temperature=0.9,
+    max_output_tokens=128,
+)
+response = chat.send_message(prompt, generation_config=generation_config)
+print(response.text)
