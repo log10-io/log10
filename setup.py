ee96a25f822618a694be89e0fc5eec2f60c12004
wenzhe <145375501+wenzhe-log10@users.noreply.github.com>
2024-03-29 09:45:43 -0700

    bump version to 0.7.3 (#128)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Log10",
-    version="0.7.2",
+    version="0.7.3",
     description="Log10 LLM data management",
     author="Log10 team",
     author_email="team@log10.io",
