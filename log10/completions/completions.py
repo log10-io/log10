45aa35fc14f46f09ff9f87c514d7f95c0f1a4729
Wenzhe Xue <wenzhe@log10.io>
2024-04-12 14:49:40 -0700

    create log10 completions report to compare models from ids or tags

--- log10/completions/completions.py
+++ log10/completions/completions.py
@@ -1,11 +1,14 @@
 import json
+import time
 
 import click
 import httpx
+import pandas as pd
 import rich
 import tqdm
 from rich.console import Console
 from rich.table import Table
+from tabulate import tabulate
 
 from log10._httpx_utils import _get_time_diff, _try_get
 from log10.llm import Log10Config
@@ -248,3 +251,246 @@ def download_completions(limit, offset, timeout, tags, from_date, to_date, compa
         res = _try_get(download_url, timeout)
         _write_completions(res, file, compact)
         pbar.update(current_batch_size)
+
+
+def _get_llm_repsone(
+    model: str,
+    messages: list[dict],
+    temperature: float = 0.2,
+    max_tokens: int = 512,
+    top_p: float = 1.0,
+):
+    ret = {"content": "", "usage": {"prompt_tokens": 0, "completion_tokens": 0, "total_tokens": 0}, "duration": 0.0}
+    # TODO: use log10 llm abstraction
+    start_time = time.perf_counter()
+    if "gpt-4" in model or "gpt-3.5" in model:
+        import openai
+
+        client = openai.OpenAI()
+        response = client.chat.completions.create(
+            model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, top_p=top_p
+        )
+        ret["content"] = response.choices[0].message.content
+        ret["usage"] = response.usage.dict()
+    elif "claude-3" in model:
+        import anthropic
+
+        system_messages = [m["content"] for m in messages if m["role"] == "system"]
+        other_messages = [m for m in messages if m["role"] != "system"]
+        system_prompt = ("\n").join(system_messages)
+
+        client = anthropic.Anthropic()
+        response = client.messages.create(
+            model=model,
+            system=system_prompt,
+            messages=other_messages,
+            max_tokens=max_tokens,
+            temperature=temperature,
+            top_p=top_p,
+        )
+        ret["content"] = response.content[0].text
+        ret["usage"]["prompt_tokens"] = response.usage.input_tokens
+        ret["usage"]["completion_tokens"] = response.usage.output_tokens
+        ret["usage"]["total_tokens"] = response.usage.input_tokens + response.usage.output_tokens
+    # elif "mistral" in model:
+    #     import mistralai
+    #     client = mistralai.client.MistralClient()
+    #     response = client.chat(
+    #         model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, top_p=top_p
+    #     )
+    #     ret["content"] = response.choices[0].message.content
+    #     ret["usage"] = response.usage.dict()
+    else:
+        raise ValueError(f"Model {model} not supported.")
+    ret["duration"] = int((time.perf_counter() - start_time) * 1000)
+
+    return ret
+
+
+def _render_comparison_table(data):
+    rich.print(f"completion_id: {data['completion_id']}")
+    rich.print("original_request:")
+    rich.print_json(json.dumps(data["original_request"], indent=4))
+
+    table = rich.table.Table(show_header=True, header_style="bold magenta", box=rich.box.ROUNDED, show_lines=True)
+    table.add_column("Model")
+    table.add_column("Content")
+    table.add_column("Usage (prompt/completion)")
+    table.add_column("Duration (ms)")
+
+    for model, data in data.items():
+        if model not in ["completion_id", "original_request"]:
+            usage = data["usage"]
+            formatted_usage = f"{usage['total_tokens']} ({usage['prompt_tokens']}/{usage['completion_tokens']})"
+            table.add_row(model, data["content"], formatted_usage, str(data["duration"]))
+    rich.print(table)
+
+
+def _create_dataframe_from_comparison_data(data):
+    # return dataframe with columns: completion_id, prompt_messages,model, content, prompt_tokens, completion_tokens, total_tokens, duration
+    # prompt_message is original_request["messages"], dumps to json with indent=4
+
+    completion_id = data["completion_id"]
+    original_request = data["original_request"]
+    rows = []
+    for model, model_data in data.items():
+        if model not in ["completion_id", "original_request"]:
+            content = model_data["content"]
+            usage = model_data["usage"]
+            prompt_tokens = usage["prompt_tokens"]
+            completion_tokens = usage["completion_tokens"]
+            total_tokens = usage["total_tokens"]
+            duration = model_data["duration"]
+            prompt_messages = json.dumps(original_request["messages"])
+            rows.append(
+                [
+                    completion_id,
+                    prompt_messages,
+                    model,
+                    content,
+                    prompt_tokens,
+                    completion_tokens,
+                    total_tokens,
+                    duration,
+                ]
+            )
+
+    df = pd.DataFrame(
+        rows,
+        columns=[
+            "Completion ID",
+            "Prompt Messages",
+            "Model",
+            "Content",
+            "Prompt Tokens",
+            "Completion Tokens",
+            "Total Tokens",
+            "Duration (ms)",
+        ],
+    )
+
+    return df
+
+
+def _compare(id: str, models: list[str], temperature: float = 0.2, max_tokens: float = 256, top_p: float = 1.0):
+    res = _get_completion(id)
+    data = res.json()["data"]
+    original_model_request = data["request"]
+    original_model_response = data["response"]
+    original_model = original_model_response["model"]
+
+    ret = {
+        "completion_id": id,
+        "original_request": original_model_request,
+        original_model: {
+            "content": original_model_response["choices"][0]["message"]["content"],
+            "usage": original_model_response["usage"],
+            "duration": data["duration"],
+        },
+    }
+    if models:
+        for model in models:
+            rich.print(f"Running {model}")
+            response = _get_llm_repsone(
+                model,
+                original_model_request["messages"],
+                temperature=temperature,
+                max_tokens=max_tokens,
+                top_p=top_p,
+            )
+
+            ret[model] = response
+
+    return ret
+
+
+@click.command()
+@click.option("--ids", default="", help="Completion ID")
+@click.option("--tags", default="", help="Filter completions by specific tags. Separate multiple tags with commas.")
+@click.option("--limit", help="Specify the maximum number of completions to retrieve.")
+@click.option("--offset", help="Set the starting point (offset) from where to begin fetching completions.")
+@click.option("--models", default="claude-3-haiku-20240307", help="Comma separated list of models to compare")
+@click.option("--temperature", default=0.2, help="Temperature")
+@click.option("--max_tokens", default=512, help="Max tokens")
+@click.option("--top_p", default=1.0, help="Top p")
+# add a option to specify the output file --file
+@click.option("--file", "-f", help="Specify the filename for the report in markdown format.")
+def report(ids, tags, limit, offset, models, temperature, max_tokens, top_p, file):
+    if ids and tags:
+        raise click.UsageError("--ids and --tags cannot be set together.")
+    if (limit or offset) and not tags:
+        raise click.UsageError("--limit and --offset can only be used with --tags.")
+    if tags:
+        if not limit:
+            limit = 5
+        if not offset:
+            offset = 0
+
+    # get completions ids
+    completion_ids = []
+    if ids:
+        completion_ids = ids.split(",")
+    elif tags:
+        base_url = _log10_config.url
+        org_id = _log10_config.org_id
+        url = _get_completions_url(limit, offset, tags, None, None, base_url, org_id)
+        res = _try_get(url)
+        completions = res.json()["data"]
+        completion_ids = [completion["id"] for completion in completions]
+
+    compare_models = models.split(",")
+    # TODO verify all models are supported
+    data = []
+    for id in completion_ids:
+        ret = _compare(id, compare_models, temperature, max_tokens, top_p)
+        data.append(ret)
+
+    # create an empty dataframe
+    all_df = pd.DataFrame(
+        columns=[
+            "Completion ID",
+            "Prompt Messages",
+            "Model",
+            "Content",
+            "Prompt Tokens",
+            "Completion Tokens",
+            "Total Tokens",
+            "Duration (ms)",
+        ]
+    )
+    # render data
+    if not file:
+        for ret in data:
+            _render_comparison_table(ret)
+    else:
+
+        def generate_results_table(
+            dataframe: pd.DataFrame, column_list: list[str] = None, section_name: str = ""
+        ) -> str:
+            selected_df = dataframe[column_list] if column_list else dataframe
+            section_name = f"## {section_name}" if section_name else "## Test Results"
+
+            table = tabulate(selected_df, headers="keys", tablefmt="pipe", showindex=True)
+            ret_str = f"{section_name}\n{table}"
+            return ret_str
+
+        def generate_markdown_report(test_name: str, report_strings: list[str]):
+            with open(test_name, "w") as f:
+                f.write(f"Generated from {test_name}.\n\n")
+                for report_string in report_strings:
+                    f.write(report_string + "\n\n")
+
+        for ret in data:
+            df = _create_dataframe_from_comparison_data(ret)
+            all_df = pd.concat([all_df, df])
+        pivot_df = all_df.pivot(index="Completion ID", columns="Model", values="Content")
+        pivot_df["Prompt Messages"] = all_df.groupby("Completion ID")["Prompt Messages"].first()
+        # Reorder the columns
+        cols = pivot_df.columns.tolist()
+        cols = [cols[-1]] + cols[:-1]
+        pivot_df = pivot_df[cols]
+
+        pivot_table = generate_results_table(pivot_df, section_name="model comparison")
+        all_results_table = generate_results_table(all_df, section_name="All Results")
+        generate_markdown_report(file, [pivot_table, all_results_table])
+        rich.print(f"Report saved to {file}")
