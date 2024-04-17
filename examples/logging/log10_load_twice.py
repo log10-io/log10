35e402aa515ac0f8599949bd523f893ec4ee39c4
Wenzhe Xue <wenzhe@log10.io>
2024-04-16 20:19:13 -0700

    add an example to test multiple log10 load only log once

---
+++ examples/logging/log10_load_twice.py
@@ -0,0 +1,28 @@
+# from log10.load import OpenAI
+import openai
+from openai import OpenAI
+
+from log10.load import log10
+
+
+log10(openai)
+log10(openai)
+
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are the most knowledgable Star Wars guru on the planet",
+        },
+        {
+            "role": "user",
+            "content": "Write the time period of all the Star Wars movies and spinoffs?",
+        },
+    ],
+)
+
+print(completion.choices[0].message)
