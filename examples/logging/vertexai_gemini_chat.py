f97d912825ea446d7bc719cbb198684555234ed4
wenzhe <145375501+wenzhe-log10@users.noreply.github.com>
2024-04-18 15:08:25 -0700

    bump versoin to 0.8.0 (#145)

--- examples/logging/vertexai_gemini_chat.py
+++ examples/logging/vertexai_gemini_chat.py
@@ -17,7 +17,7 @@ chat = model.start_chat()
 prompt = "What's the top 5 largest constellations you can find in North American during March?"
 generation_config = GenerationConfig(
     temperature=0.9,
-    max_output_tokens=128,
+    max_output_tokens=512,
 )
 response = chat.send_message(prompt, generation_config=generation_config)
 print(response.text)
