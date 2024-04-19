89202142f9e90a54f24bd3461ac3f36cb54f1fe9
Niklas Nielsen <nik@qni.dk>
2024-04-19 08:28:54 -0700

    Patch release for streaming bug

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Log10",
-    version="0.8.0",
+    version="0.8.1",
     description="Log10 LLM data management",
     author="Log10 team",
     author_email="team@log10.io",
