c74f08fdd28dad6b256a3939c2b9176702892d29
Kim Tran <ksprtran@gmail.com>
2024-05-21 14:23:14 -0400

    Update anthropic tools example

--- examples/logging/anthropic_tools_stream.py
+++ examples/logging/anthropic_tools_stream.py
@@ -9,13 +9,26 @@ log10(anthropic)
 client = anthropic.Anthropic()
 
 with client.beta.tools.messages.stream(
-    model="claude-instant-1.2",
-    messages=[
+    model="claude-3-haiku-20240307",
+    tools=[
         {
-            "role": "user",
-            "content": "Howdy",
+            "name": "get_weather",
+            "description": "Get the weather at a specific location",
+            "input_schema": {
+                "type": "object",
+                "properties": {
+                    "location": {"type": "string", "description": "The city and state, e.g. San Francisco, CA"},
+                    "unit": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                        "description": "Unit for the output",
+                    },
+                },
+                "required": ["location"],
+            },
         }
     ],
+    messages=[{"role": "user", "content": "What is the weather in SF?"}],
     max_tokens=1024,
 ) as stream:
     for message in stream:
