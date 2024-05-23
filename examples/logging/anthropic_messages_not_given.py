741a5db107e2e7fc2cc217bb4a723f7ea9e863ae
Kim Tran <ksprtran@gmail.com>
2024-05-23 18:19:33 -0400

    Add tests for new anthropic changes

--- examples/logging/anthropic_messages_not_given.py
+++ examples/logging/anthropic_messages_not_given.py
@@ -1,10 +1,12 @@
+from anthropic import NOT_GIVEN
+
 from log10.load import Anthropic
 
 
 client = Anthropic()
 
-completion = client.messages.create(
-    model="claude-instant-1.2",
+completion = client.beta.tools.messages.create(
+    model="claude-3-haiku-20240307",
     messages=[
         {
             "role": "user",
@@ -12,6 +14,8 @@ completion = client.messages.create(
         },
     ],
     max_tokens=1000,
+    tools=NOT_GIVEN,
+    tool_choice=NOT_GIVEN,
 )
 
 print(completion.content[0].text)
