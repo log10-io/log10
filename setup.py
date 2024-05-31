5851bceecdbfb41d3a3f19ddcaf7bbbb4af63b9c
Niklas Nielsen <nik@qni.dk>
2024-05-31 11:23:34 -0700

    Explicit package listing

--- setup.py
+++ setup.py
@@ -1,24 +1,42 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 
-from setuptools import find_packages, setup
-
+from setuptools import setup
 
 setup(
     name="Log10",
-    version="0.8.6",
-    description="Log10 LLM data management",
+	@@ -12,13 +11,35 @@
     author="Log10 team",
     author_email="team@log10.io",
     url="",
-    packages=find_packages(),
+    packages=[
+        "log10",
+    ],
     install_requires=[
-        "openai",
-        "python-dotenv",
+        "openai<2",
+        "anthropic<1",
+        "requests>=2.31.0",
+        "python-dotenv>=1.0.0",
+        "backoff>=2.2.1",
+        "pandas>=2",
     ],
     extras_require={
-        "bigquery": ["google-cloud-bigquery"],
-        "dev": ["chromadb"],
+        "autofeedback_icl": ["magentic>=0.17.0; python_version >= '3.10'"],
+        "litellm": ["litellm>=1.34.18"],
+        "langchain": ["langchain<0.2.0"],
+        "gemini": ["google-cloud-aiplatform>=1.44.0"],
+        "mistralai": ["mistralai>=0.1.5"],
+        "together": ["together>=0.2.7"],
+        "mosaicml": ["mosaicml-cli>=0.5.30"],
+        "google-generativeai": ["google-generativeai>=0.5.2"],
+        "bigquery": ["google-cloud-bigquery>=3.11.4"],
+        "dev": [
+            "build>=0.10.0",
+            "pytest>=8.0.0",
+            "requests-mock>=1.11.0",
+            "respx>=0.20.2",
+            "ruff>=0.3.2",
+            "pytest-asyncio>=0.23.6",
+            "chromadb"
+        ],
     },
 )
