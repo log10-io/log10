42c223b41614ee543b9721de73759ae75451783b
Niklas Nielsen <nik@qni.dk>
2024-06-11 10:52:23 -0700

    Styles

--- examples/logging/large_image_upload.py
+++ examples/logging/large_image_upload.py
@@ -1,18 +1,18 @@
-from magentic import chatprompt, SystemMessage, UserMessage
-from magentic.vision import UserImageMessage
-from log10.load import log10
 import openai
+from magentic import SystemMessage, chatprompt
+from magentic.vision import UserImageMessage
+
+from log10.load import log10
+
 
 log10(openai)
 
 with open("./examples/logging/large_image.png", "rb") as f:
     image_bytes = f.read()
 
-@chatprompt(
-        SystemMessage("What's in the following screenshot?"),
-        UserImageMessage(image_bytes)
-)
-def _llm() -> str:
-    ...
+
+@chatprompt(SystemMessage("What's in the following screenshot?"), UserImageMessage(image_bytes))
+def _llm() -> str: ...
+
 
 _llm()
