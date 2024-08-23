98ae5de80a2d6f1f6f5f8c2492690635faef4308
Kim Tran <ksprtran@gmail.com>
2024-08-22 21:56:02 -0400

    Add openai perplexity compability logging examples

---
+++ examples/logging/magentic_async_chat_openai_perplexity_compability.py
@@ -0,0 +1,26 @@
+import asyncio
+
+import openai
+from magentic import UserMessage, chatprompt
+from magentic.chat_model.litellm_chat_model import LitellmChatModel
+from log10._httpx_utils import finalize
+from log10.load import log10
+
+
+log10(openai)
+
+
+async def main(topic: str) -> str:
+    @chatprompt(
+        UserMessage(f"Tell me a joke about {topic}"),
+        model=LitellmChatModel(
+            model="perplexity/llama-3.1-sonar-small-128k-chat"
+        ),
+    )
+    async def tell_joke(topic: str) -> str: ...
+
+    print(await tell_joke(topic))
+    await finalize()
+
+
+asyncio.run(main("cats"))
