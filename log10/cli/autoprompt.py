dc56ca31c59ff77551fc14dd9967253b336ae8e9
Wenzhe Xue <wenzhe@log10.io>
2024-08-27 23:40:16 -0700

    simplify option to prompt but support three format: * string * openai chat request messages * log10 completion format

--- log10/cli/autoprompt.py
+++ log10/cli/autoprompt.py
@@ -6,45 +6,65 @@ import click
 from log10.prompt_analyzer import PromptAnalyzer, display_prompt_analyzer_suggestions
 
 
+# ignor "tool" and "funciton" roles
+ALLOWED_ROLES = {"system", "user", "assistant"}
+
+
 @click.command()
-@click.option("--prompt", "-p", help="The prompt to analyze. Provide a string or a file containing the prompt.")
 @click.option(
-    "--messages", "-m", help="The messages to analyze. Provide a JSON or JSON file of openai style messages."
+    "--prompt",
+    "-p",
+    help=(
+        "The prompt to analyze. Provide a string or a file containing the prompt. "
+        "We allow three formats:\n"
+        '1) string prompt, e.g. "Summarize this article in 3 sentences."\n'
+        '2) messages, e.g. [{"role": "user", "content": "Hello"}, {"role": "assistant", "content": "Hi"}]\n'
+        '3) log10 completion, e.g. {..., "request": {..., "messages": [{"role": "user", "content": "Hello"}, {"role": "assistant", "content": "Hi"}], ...}, "response": {...}}\n'
+    ),
 )
-def autoprompt(prompt, messages):
+def autoprompt(prompt):
     """
     Analyze a prompt or messages and provide suggestions on how to improve it.
     """
-    if prompt and messages:
-        raise click.UsageError("You can only provide either a prompt or messages, not both.")
-    if not prompt and not messages:
+    if not prompt:
         click.echo("Enter the prompt to analyze (end with Ctrl+D):")
         prompt = click.get_text_stream("stdin").read()
-        # prompt = click.prompt("Enter the prompt to analyze", type=str)
+        if not prompt:
+            raise click.BadParameter("No prompt provided.")
 
-    # prompt could be a string or a file path, if it's a file path, read the file load into a string
-    if prompt:
-        prompt_file = Path(prompt)
-        if prompt_file.exists():
-            with open(prompt_file, "r") as f:
-                prompt = f.read()
-                click.echo(f"Prompt loaded from {prompt_file}:")
-    elif messages:
-        messages_file = Path(messages)
-        if messages_file.exists():
-            # check message_file extension is json
-            if messages_file.suffix.lower() != ".json":
-                raise click.UsageError("Only .json extension is supported for the messages file.")
-            with open(messages_file, "r") as f:
-                # load messages_file to json, then extract the content of each message
-                messages = f.read()
+    # check if prompt is a file path
+    prompt_file = Path(prompt)
+    if prompt_file.exists():
+        with open(prompt_file, "r") as f:
+            click.echo(f"Prompt loaded from {prompt_file}:")
+            prompt = f.read()
 
-            try:
-                messages = json.loads(messages)
-            except json.JSONDecodeError:
-                raise click.UsageError("Invalid JSON format for messages file.")
-            click.echo(f"Messages loaded from {messages_file}:")
-        prompt = "\n\n".join([m.get("content", "") for m in messages])
+    # try to parse the prompt as a json
+    try:
+        prompt_json = json.loads(prompt)
+
+        if isinstance(prompt_json, list) and all("role" in item and "content" in item for item in prompt_json):
+            # prompt is a list of messages
+            prompt = "\n\n".join([f"{m["role"]}: {m["content"]}" for m in prompt_json if m["role"] in ALLOWED_ROLES])
+        elif isinstance(prompt_json, dict) and "request" in prompt_json and "messages" in prompt_json["request"]:
+            # prompt is a log10 completion
+            prompt = "\n\n".join(
+                [
+                    f"{m["role"]}: {m["content"]}"
+                    for m in prompt_json["request"]["messages"]
+                    if m["role"] in ALLOWED_ROLES
+                ]
+            )
+        elif isinstance(prompt_json, str):
+            # prompt is a plain string
+            prompt = prompt_json
+        else:
+            raise click.BadParameter(
+                "Unsupported JSON format. Please provide a list of messages or a log10 completion."
+            )
+    except json.JSONDecodeError:
+        # Input is a plain string
+        prompt = prompt
 
     click.echo(prompt)
     analyzer = PromptAnalyzer()
